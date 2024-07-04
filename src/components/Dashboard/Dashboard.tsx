5233f04ca3ec950040f1acd77cbbea5e29f3f41f
Jayakrishnan VR <jayakrishnanvrjk@gmail.com>
2024-07-04 21:21:51 +0530

    dashboard changes

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import { useNavigate } from "react-router-dom"
 
 function Dashboard() {
+  const navigate = useNavigate();
+
   return (
-    <div>Dashboard</div>
-  )
+    <>
+      <div>Dashboard</div>
+      <div>
+        <button onClick={() => navigate('counter')}>Counter</button>
+        <button onClick={() => navigate('todo-list')}>TodoList</button>
+      </div>
+    </>
+  );
 }
 
-export default Dashboard
+export default Dashboard;
